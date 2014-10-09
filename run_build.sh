0809e3305770305f6e2f504bc11805dbf0b760b0
Florian Walch <florian@fwalch.com>
2014-10-09 12:05:00 +0200

    Test with nightly build

--- run_build.sh
+++ run_build.sh
@@ -1,26 +1,16 @@
-#!/bin/bash
-
-set -e
-
-source <(curl -fsSL https://raw.githubusercontent.com/neovim/neovim/master/.ci/common.sh)
-
-set_environment /opt/neovim-deps
+#!/bin/bash -e
 
 if [ ! -d $TRAVIS_BUILD_DIR/_neovim ]
 then
-  git clone https://github.com/neovim/neovim.git $TRAVIS_BUILD_DIR/_neovim
+  mkdir -p $TRAVIS_BUILD_DIR/_neovim
+  wget -q -O - https://github.com/fwalch/neovim/releases/download/nightly/neovim-x64-nightly.tar.gz \
+    | tar xzf - --strip-components=1 -C $TRAVIS_BUILD_DIR/_neovim
 fi
-
-pushd $TRAVIS_BUILD_DIR/_neovim
-git fetch origin
-git rebase origin/master
-make
-popd
 
 pushd $TRAVIS_BUILD_DIR/_cmd/gen_neovim_api/
 go get -d -t -v ./... && go build -v ./...
 x=`mktemp`
-NEOVIM_BIN=$TRAVIS_BUILD_DIR/_neovim/build/bin/nvim ./gen_neovim_api -g | gofmt > $x
+NEOVIM_BIN=$TRAVIS_BUILD_DIR/_neovim/bin/nvim ./gen_neovim_api -g | gofmt > $x
 diff $x ../../gen_client_api.go
 if [ $? -ne 0 ]
 then
@@ -30,5 +20,5 @@ fi
 rm $x
 popd
 
-NEOVIM_BIN=$TRAVIS_BUILD_DIR/_neovim/build/bin/nvim go test
-NEOVIM_BIN=$TRAVIS_BUILD_DIR/_neovim/build/bin/nvim go test -race
+NEOVIM_BIN=$TRAVIS_BUILD_DIR/_neovim/bin/nvim go test
+NEOVIM_BIN=$TRAVIS_BUILD_DIR/_neovim/bin/nvim go test -race
